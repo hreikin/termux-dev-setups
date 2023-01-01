13021cd527d2e39419880ad4379f3241ae6020a0
Michael Haslam <hreikin@gmail.com>
2023-01-01 21:15:24 +0000

    Add missing chmod command.

--- full.sh
+++ full.sh
@@ -9,12 +9,13 @@ set -eu -o pipefail
 
 # Download minimal install script.
 wget https://github.com/hreikin/termux-dev-setups/minimal.sh
+chmod +x minimal.sh
 source minimal.sh
 
-# Print welcome message.
+# Print welcome message and install extra packages.
 echo """Full installation starting.
-Installing required packages."""
-
+Installing extra packages."""
+pkg install -y 
 
 # Confirm successful installation and exit.
 echo '''Full installation complete.
