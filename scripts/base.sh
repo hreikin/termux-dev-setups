118174958bc41ff03ee40f00e1fe8ac1cbe379c3
Michael Haslam <hreikin@gmail.com>
2023-01-11 16:25:49 +0000

    Use triple quotes.

--- scripts/base.sh
+++ scripts/base.sh
@@ -37,4 +37,4 @@ ln -s $HOME/storage/downloads/ $HOME/Downloads
 ln -s $HOME/storage/dcim/ $HOME/Pictures
 
 # Add custom PS1 to bashrc.
-echo "export PS1="\[\033[38;5;11m\]\u\[$(tput sgr0)\]@\h:\[$(tput sgr0)\]\[\033[38;5;6m\][\w]\[$(tput sgr0)\]: \[$(tput sgr0)\]"" >> $HOME/.bashrc
+echo """export PS1="\[\033[38;5;11m\]\u\[$(tput sgr0)\]@\h:\[$(tput sgr0)\]\[\033[38;5;6m\][\w]\[$(tput sgr0)\]: \[$(tput sgr0)\]"""" >> $HOME/.bashrc
