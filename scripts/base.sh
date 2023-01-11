566997b3d2ba9d7bddf667b80baaae56eecacad6
Michael Haslam <hreikin@gmail.com>
2023-01-11 15:10:10 +0000

    Add custom PS1 prompt to .bashrc file.

--- scripts/base.sh
+++ scripts/base.sh
@@ -35,3 +35,6 @@ mkdir $HOME/Music
 ln -s $HOME/storage/music/ $HOME/Music
 ln -s $HOME/storage/downloads/ $HOME/Downloads
 ln -s $HOME/storage/dcim/ $HOME/Pictures
+
+# Add custom PS1 to bashrc.
+echo "export PS1="\[\033[38;5;11m\]\u\[$(tput sgr0)\]@\h:\[$(tput sgr0)\]\[\033[38;5;6m\][\w]\[$(tput sgr0)\]: \[$(tput sgr0)\]"" >> $HOME/.bashrc
